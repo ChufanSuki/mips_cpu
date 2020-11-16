bc2cd3d3bccdb4faa243ea673886553cfbf5e7cb
RetownPlato <allenplato28@gmail.com>
2020-11-16 20:01:13 +0800

    messed mux2

--- rtl/mux2.v
+++ rtl/mux2.v
@@ -1,10 +1,9 @@
-                alu_op    = 2'b00;
 module mux2 
 #(parameter p_nbits = 8) (
 	input logic [p_nbits-1:0] d0,
   input logic [p_nbits-1:0] d1,
 	input logic s,
-	output logic [p_nbits-1:0] y
+	output wire [p_nbits-1:0] y
     );
 	
 	assign y = s ? d1 : d0;
